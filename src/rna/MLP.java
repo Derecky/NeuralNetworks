a0a1cdf70adf567defb32bd078029f3db2b2ac0a
derecky costa <dereckycosta@gmail.com]>
2019-07-27 15:11:31 -0300

    perceptron simples

---
+++ src/rna/MLP.java
@@ -0,0 +1,13 @@
+package rna;
+
+public class MLP {
+
+    private double bias;
+    private double learningRate;
+    private int maxLoops;
+
+    private boolean momentum;
+    private double momentumRate;
+    private double lambda;
+    private boolean gravar;
+}
